cb0a9993596e43320578db567234553c8f729983
Louisa Goncharenko <louisa.goncharenko@shopify.com>
2023-01-25 11:36:14 -0500

    created two tables

--- main.sql
+++ main.sql
@@ -22,5 +22,36 @@ CREATE TABLE Goals (
     FOREIGN KEY (client_id) REFERENCES Clients(id)
 );
 -- Create table for Workout plans
+CREATE TABLE Workout_Plan (
+    id int NOT NULL AUTO_INCREMENT,
+    client_id int NOT NULL,
+    duration varchar(100) NOT NULL,
+    workout_split varchar(100) NOT NULL,
+    sessions_per_week int NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (client_id) REFERENCES Clients(id)
+);
 -- Create table for Workout plan exerscises
+CREATE TABLE Workout_Plan_Exerscise (
+   id int NOT NULL AUTO_INCREMENT, 
+   exercise_id int NOT NULL,
+   Workout_plan_id int NOT NULL,
+   sets int NOT NULL,
+   reps int NOT NULL,
+   PRIMARY KEY (id),
+   FOREIGN KEY (exercise_id) REFERENCES Exercises(id),
+   FOREIGN KEY (Workout_plan_id) REFERENCES Workout_Plan(id)
+);
 -- Create table for Exerscises
+CREATE TABLE Exercises (
+    id int NOT NULL AUTO_INCREMENT, 
+    name varchar(100) NOT NULL,
+    video varchar(100) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- Insert into CLIENTS
+-- Insert into GOALS
+-- Insert into WORKOUT_PLAN
+-- Insert into EXERCISES
+-- Insert into WORKOUT_PLAN_EXERCISES
