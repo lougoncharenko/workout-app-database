af3665ce29027c99768d73a864505c2d2f7e0b3c
Louisa Goncharenko <louisa.goncharenko@shopify.com>
2023-01-25 11:59:35 -0500

    inserted data into clients table

--- main.sql
+++ main.sql
@@ -51,6 +51,20 @@ CREATE TABLE Exercises (
 );
 
 -- Insert into CLIENTS
+INSERT INTO Clients 
+(full_name, dob, address, weight, height)
+VALUES 
+('Cindy Crawford', '1989-12-01', '5312 12th street MN USA', 215.03, 5.9),
+('Luke Grahamn', '1985-02-13', '6002 Lime street CA USA', 270.12, 6.1),
+('Kate Parrie', '2000-10-23', '11295 Hand Blvd FL USA', 196.1, 5.3),
+('Demi Lotato', '1999-05-05', '912 Viking Blvd TN USA', 200.7, 5.11),
+('Nicky Jonase', '1997-01-11', '5531 Bahia Vista street NY USA', 215.8, 5.7),
+('Kim Cardashion', '1995-11-17', '52191 34th Ave GA USA', 279.9, 5.11),
+('Courtnee Cardashion', '1996-09-16', '52191 34th Ave GA USA', 267.3, 5.9),
+('Timothy Cardashion', '1994-01-23', '52191 34th Ave GA USA', 320.5, 5.8),
+('Taylor Swept', '1990-02-16', '900 Charlotte Ave NC USA', 219.9, 5.7),
+('Dwayne Johnseen', '1996-01-01', '8234 Angel street  WA USA', 220.3, 6.2); 
+
 -- Insert into GOALS
 -- Insert into WORKOUT_PLAN
 -- Insert into EXERCISES
